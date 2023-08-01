590ebf456c8c6a8677947bd2f66730733afa977c
anusha-c18 <anushac2001@gmail.com>
2023-08-01 16:09:38 +0530

    add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -5,6 +5,18 @@ const projectsData = [
     imgSrc: '/static/images/projects/fabrilab.png',
     href: '/blog/projects/fabrilab-web-agency',
   },
+  {
+    title: 'Save A Life Maps',
+    description: `Creation of a web design agency.`,
+    imgSrc: '/static/images/projects/fabrilab.png',
+    href: '/blog/projects/fabrilab-web-agency',
+  },
+  {
+    title: 'Save A Life Maps',
+    description: `Creation of a web design agency.`,
+    imgSrc: '/static/images/projects/fabrilab.png',
+    href: '/blog/projects/fabrilab-web-agency',
+  },
 ];
 
 export default projectsData;
