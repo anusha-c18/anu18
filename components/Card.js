e775cd173928a8b506458838ba67d3d2940deaca
anusha-c18 <anushac2001@gmail.com>
2023-08-17 11:57:08 +0530

    style: resize cards

--- components/Card.js
+++ components/Card.js
@@ -2,7 +2,7 @@ import Image from './Image';
 import Link from './Link';
 
 const Card = ({ title, description, imgSrc, href }) => (
-  <div className="md p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
+  <div className="md p-4 md:w-1/2" style={{ maxWidth: '300px', margin: 'auto' }}>
     <div
       className={`${
         imgSrc && 'h-full'
@@ -15,8 +15,8 @@ const Card = ({ title, description, imgSrc, href }) => (
               alt={title}
               src={imgSrc}
               className="object-cover object-center md:h-36 lg:h-48"
-              width={400}
-              height={400}
+              width={300}
+              height={300}
             />
           </Link>
         ) : (
@@ -28,8 +28,8 @@ const Card = ({ title, description, imgSrc, href }) => (
             height={306}
           />
         ))}
-      <div className="p-6">
-        <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
+      <div className="p-3">
+        <h2 className="mb-2 mt-1 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
             <Link href={href} aria-label={`Link to ${title}`}>
               {title}
@@ -38,7 +38,9 @@ const Card = ({ title, description, imgSrc, href }) => (
             title
           )}
         </h2>
-        <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
+        <p className="prose mb-2 max-w-none font-medium text-gray-500 dark:text-gray-400">
+          {description}
+        </p>
         {href && (
           <Link
             href={href}
