ad96542cbb4f00a04b20a50f9291f5ffdc5a3295
anusha-c18 <anusha.chaturvedi@hpe.com>
2023-09-17 03:59:15 +0530

    feat: add to project data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Resource Booking App',
+    description: `Resource Booking Made Easy!`,
+    imgSrc: '/static/images/projects/resource booking app/logo.jpg',
+    href: '/blog/projects/resource-booking-app',
+  },
+  {
     title: 'Save A Life Maps',
     description: `Fastest Emergency Service Provider enabled by the Novel Path Clearance Algorithm.`,
     imgSrc: '/static/images/projects/sal maps/sal.jpeg',
